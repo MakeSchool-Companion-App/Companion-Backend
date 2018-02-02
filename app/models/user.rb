1628abf157281dcfb68f6460cf2ccc0c909fa913
Matthew Harrilal <matthewharrilal@gmail.com>
2018-02-02 02:30:09 -0800

    Wrote the tests for the user class as well as testing the relationships between the class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
     has_many :attendance  
+    validates :password, presence: true
+    validates :email, presence:true, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do 
+    it "is valid with valid attributes" do
+    user = User.new(email: "peterpan@gmail.com", password: "neverland")
+    expect(user).to be_valid
+    end
+
+    # This test is going to test if a user is trying to sign up without creating a email for themself
+    it "is invalid without a email" do 
+    user = User.new(email:nil, password: "Oop")
+    expect(user).to_not be_valid
+    end
+
+    it "is invalid without a password" do 
+      user = User.new(email: "jadestone@gmail.com", password: nil)
+      expect(user).to_not be_valid
+    end
+
+    it "is invalid without email and password" do 
+      user = User.new(email:nil, password:nil)
+      expect(user).to_not be_valid
+    end
+
+  describe "Associations" do 
+    it "users should have many days of attendances" do 
+      association = User.reflect_on_association(:attendance)
+      expect(association.macro).to eq :has_many
+  end
+end
+
+  end
 end
