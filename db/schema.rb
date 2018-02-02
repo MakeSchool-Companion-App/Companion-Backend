e70b629cfbb68682a3dcc2c5a8530cc296cf730c
Matthew Harrilal <matthewharrilal@gmail.com>
2018-02-02 00:16:54 -0800

    Added the controller with the routes for the attendance

--- db/schema.rb
+++ db/schema.rb
@@ -21,8 +21,12 @@ ActiveRecord::Schema.define(version: 2018020122404011) do
     t.string "out_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_attendances_on_user_id"
+  end
+
+  create_table "controllers", force: :cascade do |t|
+    t.string "Attendance"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,5 +38,4 @@ ActiveRecord::Schema.define(version: 2018020122404011) do
     t.index ["password"], name: "index_users_on_password"
   end
 
-  add_foreign_key "attendances", "users"
 end
