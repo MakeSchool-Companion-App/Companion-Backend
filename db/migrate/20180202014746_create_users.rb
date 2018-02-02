f5b026bfd119a6344adfaf0717b892176f41111f
Matthew Harrilal <matthewharrilal@gmail.com>
2018-02-01 17:49:18 -0800

    Fixed the unique password

---
+++ db/migrate/20180202014746_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password
+
+      t.timestamps
+    end
+    add_index :users, :password
+    add_index :users, :email, unique: true
+  end
+end
+
+
+
