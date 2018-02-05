3a162dbf9b1f541f1debbaa7b5442c1bd19febee
Matthew Harrilal <matthewharrilal@gmail.com>
2018-02-05 14:12:44 -0800

    Starting to implement the devise library for authentication

--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Attendance, type: :model do
   subject {
-      User.new(id: nil, email: "matthewharrilal@gmail.com", password: "matthewharrilal", created_at: nil, updated_at: nil)
+      User.new(id: 1, email: "matthewharrilal@gmail.com", password: "matthewharrilal", created_at: nil, updated_at: nil)
   }
   describe "Validations" do 
-    # it "must have in time and associated with user id" do
-     
-    #   attendance 
-    #   expect(attendance).to be_valid
-    # end 
+  #   it "must have in time and associated with user id" do
+      
+  #     attendance = Attendance.new(name: subject.email, in_time: "1:35pm", out_time:nil, created_at:nil, updated_at: nil, user_id: subject.id)
+  #     expect(attendance).to be_valid
+  #   end 
 
     it "has in time and no associated user id" do 
       # We are testing for if the user has passed the only requeired parameter and did not associate the attendance with the user id
