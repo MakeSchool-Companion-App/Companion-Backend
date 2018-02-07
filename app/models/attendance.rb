6f40cccffdf9224de4f106238c8e3e8f0a102b7d
Matthew Harrilal <matthewharrilal@gmail.com>
2018-02-06 19:40:11 -0800

    made some changes

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,7 +2,7 @@ class Attendance < ApplicationRecord
     # In our migration file are the attributes for the attendance class which are the name of the user as well as 
     # the in time and the out time
     belongs_to :user
-    validates :in_time, presence:true
+    validates :event, :event_time, presence:true
     validates :beacon_id, presence: true
     validates :user_id, presence:true, uniqueness:true
 end
