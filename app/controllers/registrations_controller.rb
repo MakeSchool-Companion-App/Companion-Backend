6f08f63f067d22351b9832fc0c03f4f8a369bb60
Matthew Harrilal <matthewharrilal@gmail.com>
2018-03-10 08:25:12 -0800

    Got the json to render as one object

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -21,12 +21,14 @@ class RegistrationsController < ApplicationController
     
     if user != nil  
       hashable_user = user.as_json
-      user_email = hashable_user[:email]
-      user_image_url = hashable_user[:profile_image_url]
-      user_first_name = hashable_user[:first_name]
-      user_last_name = hashable_user[:last_name]
 
-      @newUser = User.new(email: params[:email])
+      user_image_url = hashable_user['profile_image_url']
+      p user_image_url
+      user_first_name = hashable_user['first_name']
+      user_last_name = hashable_user['last_name']
+      
+
+      @newUser = User.new(email: params[:email], image_url: user_image_url, first_name: user_first_name, last_name: user_last_name)
       @newUser.save
       render json: @newUser
     end
