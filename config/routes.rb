6bb8c049b5a673f3563b3d2092b78c65ebced6aa
Matthew Harrilal <matthewharrilal@gmail.com>
2018-05-03 21:20:51 -0700

    Added the new route for the facebook users

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :active_sessions
   resources :registrations
   resources :users
+  resources :users do 
+    post "facebook_users"
+  end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
