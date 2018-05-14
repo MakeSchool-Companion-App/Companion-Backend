bb1e2fce9a8def92af00161087557842a6aea51c
Matthew Harrilal <matthewharrilal@gmail.com>
2018-05-14 12:31:00 -0700

    Made changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,12 +13,12 @@ class UsersController < ApplicationController
     # user_token.slice!(0,12)
     # @user = User.where({"token":user_token})
     # p user_token
-    @user = User.find_by({'email': params[:email]})
-    if @user.present?
-      render json: @user
-    else:
-      @user = User.find_by({'email': params[:email]})
-      render json: @user
+    # @user = User.find_by({'email': params[:email]})
+    # if @user.present?
+    #   render json: @user
+    # else:
+    #   @user = User.find_by({'email': params[:email]})
+    #   render json: @user
     
     end
 
@@ -66,6 +66,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.permit(:email, :password, :first_name, :userame, :image_url, :last_name)
+      params.permit(:email, :password, :first_name, :username, :image_url, :last_name)
     end
 end
