441ae3a6eefe83a00f84e5eb85eda6e08ba67de7
LinnierGames <esericksanc@gmail.com>
2018-10-12 20:13:09 -0700

    WIP: Update Beacon Test after Merging

--- rspec/test/models/beacon_test.rb
+++ rspec/test/models/beacon_test.rb
@@ -4,4 +4,10 @@ class BeaconTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "contains all properties" do
+    beacon = beacons(:valid)
+
+    assert User.new(beacon)
+  end
 end
