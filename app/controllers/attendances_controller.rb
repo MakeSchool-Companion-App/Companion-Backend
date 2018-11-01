1be28d5a2ae3501ee2b670a97bd5cf4801d7a60d
Matthew Harrilal <matthewharrilal@gmail.com>
2018-11-01 15:02:13 -0700

    MAde changes

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -19,7 +19,7 @@ class AttendancesController < ApplicationController
     def create
         '''Create an attendance object belonging to the current user '''
         beacon = Beacon.find_by({title: attendance_params[:beacon_id]})
-        attendance_params[:beacon_id] = beacon.id
+        params[:beacon_id] = beacon.id
         puts 'New attendance params %s' %(attendance_params[:beacon_id])
         @attendance = Attendance.new(attendance_params)
         @attendance.user = current_user
