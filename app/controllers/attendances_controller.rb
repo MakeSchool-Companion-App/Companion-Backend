88b6aea950260df57cc1c8d0c91ae3a1a4624fe1
Matthew Harrilal <matthewharrilal@gmail.com>
2018-11-03 11:22:54 -0700

    Casting beacon id to str

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -23,7 +23,7 @@ class AttendancesController < ApplicationController
         @attendance.user = current_user
 
         if @attendance.save
-            casted_beacon_id = {beacon_id: @attendance.beacon_id.to_s, id: @attendance.id, created_at: @attendance.created_at, updated_at: @attendance.updated_at]}
+            casted_beacon_id = {beacon_id: @attendance.beacon_id, id: @attendance.id, created_at: @attendance.created_at, updated_at: @attendance.updated_at]}
             puts 'This is the casted beacon id %s' %(casted_beacon_id)
             render json: casted_beacon_id
         else
