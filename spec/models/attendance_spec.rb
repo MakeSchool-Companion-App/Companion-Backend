51ce0eb09f0dc739392bda069a3f3bbbefdd8a54
LinnierGames <esericksanc@gmail.com>
2018-11-12 22:59:28 -0800

    WIP: Attendance Model

--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -1,20 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Attendance, type: :model do
-  subject {
-      User.new(id: 1, email: "matthewharrilal@gmail.com", password: "matthewharrilal", created_at: nil, updated_at: nil)
-  }
-  describe "Validations" do 
-  #   it "must have in time and associated with user id" do
-      
-  #     attendance = Attendance.new(name: subject.email, in_time: "1:35pm", out_time:nil, created_at:nil, updated_at: nil, user_id: subject.id)
-  #     expect(attendance).to be_valid
-  #   end 
+  let(:user) { User.new(id: 1, email: "matthewharrilal@gmail.com", password: "matthewharrilal", created_at: nil, updated_at: nil) }
 
-    it "has in time and no associated user id" do 
-      # We are testing for if the user has passed the only requeired parameter and did not associate the attendance with the user id
-      attendance = Attendance.new(name: nil, in_time: "1:35pm", out_time:nil, created_at:nil, updated_at: nil, user_id: nil)
-      expect(attendance).to_not be_valid
-    end 
+  let(:beacon) { Beacon.new(hardware_id: "1245_dfwfe", title: "Hallway") }
+
+  subject {
+    Attendance.new(event_in: "12pm", event_out: "4pm", beacon: beacon, user: user)
+  }
+
+  describe "Validations" do
+    it "is valid with all attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is invalid without event_in" do
+      subject.event_in = nil
+
+      expect(subject).to_not be_valid
+    end
+
+    it "is valid without event_out" do
+      subject.event_out = nil
+
+      expect(subject).to be_valid
+    end
+
+    it "is valid without beacon" do
+      subject.beacon_id = nil
+
+      expect(subject).to_not be_valid
+    end
+
+    it "is valid without user" do
+      subject.user_id = nil
+
+      expect(subject).to_not be_valid
+    end
   end
 end
