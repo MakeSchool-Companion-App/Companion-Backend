d0a4bf1460146f7ee9e2948e701bca51440c1696
LinnierGames <esericksanc@gmail.com>
2018-11-12 22:39:27 -0800

    WIP: User Model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,37 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "Validations" do 
+  subject {
+    User.new(
+      email: "peterpan@gmail.com",
+      password: "neverland",
+      token: "thisIsAValidToken",
+      image_url: "https://www.google.com",
+      first_name: "Erick",
+      last_name: "Sanchez",
+      user_id: "999999"
+    )
+  }
+
+  describe "Validations" do
     it "is valid with valid attributes" do
-    user = User.new(email: "peterpan@gmail.com", password: "neverland")
-    expect(user).to be_valid
+      expect(subject).to be_valid
     end
 
     # This test is going to test if a user is trying to sign up without creating a email for themself
-    it "is invalid without a email" do 
-    user = User.new(email:nil, password: "Oop")
-    expect(user).to_not be_valid
+    it "is invalid without a email" do
+      subject.email = nil
+
+      expect(subject).to_not be_valid
     end
 
-    it "is invalid without a password" do 
-      user = User.new(email: "jadestone@gmail.com", password: nil)
-      expect(user).to_not be_valid
+    it "is invalid without a token" do
+      subject.token = nil
+
+      expect(subject).to_not be_valid
     end
 
-    it "is invalid without email and password" do 
-      user = User.new(email:nil, password:nil)
-      expect(user).to_not be_valid
+    it "is invalid without a first_name" do
+      subject.first_name = nil
+
+      expect(subject).to_not be_valid
     end
 
-  describe "Associations" do 
-    it "users should have many days of attendances" do 
+    it "is invalid without a last_name" do
+      subject.last_name = nil
+
+      expect(subject).to_not be_valid
+    end
+
+    it "is invalid without a image_url" do
+      subject.image_url = nil
+
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe "Associations" do
+    it "users should have many attendances" do
       association = User.reflect_on_association(:attendance)
+
       expect(association.macro).to eq :has_many
+    end
+
+    it "users should have many beacons through attendances" do
+      association = User.reflect_on_association(:beacon)
+
+      expect(association.macro).to eq :has_many
+    end
   end
-
 end
-
-  end
-end
-
-
