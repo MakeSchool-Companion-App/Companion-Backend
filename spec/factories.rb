0e15056a93234411d7c04a0658db7a34f19c6f4d
LinnierGames <esericksanc@gmail.com>
2018-11-14 19:37:01 -0800

    Beacon Request Tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,4 +8,12 @@ FactoryBot.define do
 		last_name { "Sanchez" }
 		user_id { "999999" }
 	end
+
+	factory :beacon do
+		hardware_id { "1245_dfwfe" } 
+		title { "Hallway" }
+	end
+
+	factory :beacon_invalid, class: Beacon do
+	end
 end
